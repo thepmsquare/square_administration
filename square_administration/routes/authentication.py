7e35f21e934f4bc5f28d7abfeca64b5263bbfd10
Parth Mangtani <impmmmm@gmail.com>
2024-11-17 13:59:43 +0530

    :necktie: add endpoint to remove_app_for_self in square_admin

--- square_administration/routes/authentication.py
+++ square_administration/routes/authentication.py
@@ -1,7 +1,8 @@
 import json
+from typing import Annotated
 
 import bcrypt
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status, HTTPException, Header
 from fastapi.responses import JSONResponse
 from requests import HTTPError
 from square_commons import get_api_output_in_standard_format
@@ -134,6 +135,70 @@ async def login_username_v0(
         """
         output_content = get_api_output_in_standard_format(
             message=messages["LOGIN_SUCCESSFUL"],
+            data={"main": response["data"]["main"]},
+        )
+        return JSONResponse(
+            status_code=status.HTTP_200_OK,
+            content=output_content,
+        )
+    except HTTPError as http_error:
+        global_object_square_logger.logger.error(http_error, exc_info=True)
+        """
+        rollback logic
+        """
+        # pass
+        return JSONResponse(
+            status_code=http_error.response.status_code,
+            content=json.loads(http_error.response.content),
+        )
+    except HTTPException as http_exception:
+        global_object_square_logger.logger.error(http_exception, exc_info=True)
+        """
+        rollback logic
+        """
+        # pass
+        return JSONResponse(
+            status_code=http_exception.status_code, content=http_exception.detail
+        )
+    except Exception as e:
+        global_object_square_logger.logger.error(e, exc_info=True)
+        """
+        rollback logic
+        """
+        # pass
+        output_content = get_api_output_in_standard_format(
+            message=messages["GENERIC_500"],
+            log=str(e),
+        )
+        return JSONResponse(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=output_content
+        )
+
+
+@router.patch("/remove_app_for_self/v0")
+@global_object_square_logger.async_auto_logger
+async def remove_app_for_self_v0(
+    access_token: Annotated[str, Header()],
+):
+
+    try:
+        """
+        validation
+        """
+        # pass
+        """
+        main process
+        """
+        response = global_object_square_authentication_helper.update_user_app_ids_v0(
+            access_token=access_token,
+            app_ids_to_add=[],
+            app_ids_to_remove=[global_int_app_id],
+        )
+        """
+        return value
+        """
+        output_content = get_api_output_in_standard_format(
+            message=messages["GENERIC_UPDATE_SUCCESSFUL"],
             data={"main": response["data"]["main"]},
         )
         return JSONResponse(
